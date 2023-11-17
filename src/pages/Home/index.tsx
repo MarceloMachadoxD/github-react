0a45c07ebb0366a5b8ca264548d2c52de9c0877a
Marcelo Machado <marcelo.araujo@evoluservices.com>
2023-11-17 16:11:28 -0300

    Update styles and text on Home page and Navbar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,8 +5,10 @@ import './styles.css';
 const Home = () => {
     return (
         <div className="home-container">
+            <h1 className="text-title">Desafio Github API</h1>
+            <h6 className="text-subtitle">Bootcamp Spring React</h6>
             <Link to="/">
-                <button className="btn btn-primary btn-lg start-button">Iniciar</button>
+                <button className="btn btn-primary btn-lg start-button">Começar</button>
             </Link>
         </div>
     );
